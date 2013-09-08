4631c6fdecef1cf4762384183f7588156146e8b7
mwong56 <michael.wong110@gmail.com>
2013-09-08 16:14:59 -0700

    sending mail

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,6 +8,7 @@ class ContactsController < ApplicationController
     @contact = Contact.new(secure_params)
     if @contact.valid?
       @contact.update_spreadsheet
+      UserMailer.contact_email(@contact).deliver
       # TODO send message
       flash[:notice] = "Message sent from #{@contact.name}."
       redirect_to root_path
