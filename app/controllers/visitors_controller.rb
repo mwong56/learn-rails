9d075c6ddd1aa6136ea68f17062b0b02a0c82f4b
mwong56 <michael.wong110@gmail.com>
2013-09-08 15:56:30 -0700

    save data to a spreadsheet

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,8 +2,6 @@ class VisitorsController < ApplicationController
 
   def show
     @owner = Owner.new
-    flash.now[:notice] = 'Welcome!'
-    flash.now[:alert] = 'My birthday is soon.'
   end
 
 end
